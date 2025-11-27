b229b0922ae07e163737541f58f27e95308c7c7d
alymarquez <alyym.55@gmail.com>
2025-11-27 03:09:36 -0300

    Project section base

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,17 @@
-import Header from '../components/Header.jsx'
-import Hero from '../components/Hero.jsx'
-import Services from '../components/Services.jsx'
+import Header from "../components/Header.jsx";
+import Hero from "../components/Hero.jsx";
+import Project from "../components/Project.jsx";
+import Services from "../components/Services.jsx";
 
 function Home() {
-  return (<>
-    <Header />
-    <Hero />
-    <Services />
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Services />
+      <Project />
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
