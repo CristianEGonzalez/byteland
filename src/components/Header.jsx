55336e90cf829e7fa258e98a35d47f912cdeaaad
CristianEGonzalez <cristianarg_87@hotmail.com>
2025-11-30 16:02:23 -0300

    Menu hamburguesa

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,12 @@
+import { useState } from "react";
+
 const Header = () => {
-return (
+  const [open, setOpen] = useState(false);
+  const hambugerLine = `h-1 w-6 my-1 rounded-full bg-linear-to-r from-brand-cyan to-brand-purple transition ease transform duration-300`;
+
+  const links = ['Servicios', 'Nosotros', 'Proyectos', 'FAQ']
+
+  return (
     <header className="sticky top-0 z-100 w-full bg-brand-black py-3 px-4 pb-3 flex justify-between items-center border-b border-gray-900/50 font-mono">
       {/* Logo Area */}
       <div className="flex items-center text-4xl tracking-tighter">
@@ -11,7 +18,7 @@ return (
 
       {/* Navigation Links */}
       <nav className="hidden md:flex items-center ">
-        {['Servicios', 'Proyectos', 'Nosotros', 'FAQ'].map((item) => (
+        {links.map((item) => (
           <a
             key={item}
             href={`#${item.toLowerCase()}`}
@@ -20,21 +27,65 @@ return (
             {item}
           </a>
         ))}
-        
+
         {/* Presupuesto Botón */}
-        <a 
-        href="mailto:contacto@byteland.com?subject=Solicitud de Presupuesto&body=Hola equipo de Byteland, me gustaría cotizar un proyecto..."
-        className="bg-linear-to-r from-cyan-400 to-purple-500 text-black font-bold py-2 px-6 rounded-full hover:shadow-[0_0_15px_rgba(34,211,238,0.5)] transition-all duration-300 transform hover:scale-105">
+        <a
+          href="mailto:contacto@byteland.com?subject=Solicitud de Presupuesto&body=Hola equipo de Byteland, me gustaría cotizar un proyecto..."
+          className="bg-linear-to-r from-cyan-400 to-purple-500 text-black font-bold py-2 px-6 rounded-full hover:shadow-[0_0_15px_rgba(34,211,238,0.5)] transition-all duration-300 transform hover:scale-105">
           Solicitar presupuesto
         </a>
       </nav>
 
       {/* Mobile Menu Icon */}
       <button
-        className="md:hidden text-white cursor-pointer"
-        aria-label="Abrir menú de navegación">
-        ☰
+        className="md:hidden flex flex-col h-12 w-12 justify-center items-center group"
+        aria-label="Abrir menú de navegación"
+        onClick={() => setOpen(!open)}
+      >
+        <div
+          className={`${hambugerLine} ${open
+            ? "rotate-45 translate-y-3 opacity-100 group-hover:opacity-100"
+            : "opacity-100 group-hover:opacity-100"
+            }`}
+        />
+        <div
+          className={`${hambugerLine} ${open ? "opacity-0" : "opacity-100 group-hover:opacity-100"
+            }`}
+        />
+        <div
+          className={`${hambugerLine} ${open
+            ? "-rotate-45 -translate-y-3 opacity-100 group-hover:opacity-100"
+            : "opacity-100 group-hover:opacity-100"
+            }`}
+        />
       </button>
+
+      {open && (
+        <div className="absolute top-full right-0 h-screen w-[100%] backdrop-blur-xl bg-black/10 border-l border-gray-700 flex flex-col items-center gap-10 pt-14 md:hidden">
+
+          {links.map((item) => (
+            <a
+              key={item}
+              href={`#${item.toLowerCase()}`}
+              onClick={() => setOpen(false)}
+              className="text-gray-200 text-xl font-light tracking-widest relative pb-1 hover:text-cyan-400 transition-all duration-300 after:absolute after:left-0 after:-bottom-1 after:w-0 after:h-px after:bg-gradient-to-r from-cyan-400 to-purple-500 hover:after:w-full after:transition-all after:duration-300">
+              {item}
+            </a>
+          ))}
+
+          <a
+            href="mailto:contacto@byteland.com?subject=Solicitud de Presupuesto&body=Hola equipo de Byteland, me gustaría cotizar un proyecto..."
+            className="bg-linear-to-r from-cyan-400 to-purple-500 text-black font-bold py-3 px-2 rounded-full hover:shadow-[0_0_15px_rgba(34,211,238,0.5)] transition-all duration-300 transform hover:scale-105"
+            onClick={() => setOpen(false)}
+          >
+            Solicitar presupuesto
+          </a>
+
+        </div>
+      )}
+
+
+
     </header>
   );
 };
